df84dd9a744fac47047c553fc5957fa59b4122b5
Jose Campos <130111367+suportecnico@users.noreply.github.com>
2025-01-05 00:00:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function oncar () {
     ctvei.innerHTML = '<p>Aqui vamos colocar as descrições das manutenções</p>'
     ctvei.style.textAlign = 'justify'
     ctvei.style.textIndent = '20px'
-    ivei.src = '/imagens/motor.jpg'
+    ivei.src = '../imagens/motor.jpg'
 }
 function offcar() {
     ctvei.innerHTML = '<p>Aqui veremos os gabaritos de manutenções de <strong>motores</strong>, centrais <strong>automatizadoras (placas)</strong> e seus acessórios, como por exemplo, uma <strong>fotocélula</strong>.</p>'
